57829fb5b5eac4f79ada43e16cd0bcf93dc94354
Marcel Kulina <me@brokenbytes.dev>
2026-05-15 14:51:21 +0200

    Initial Commit

---
+++ SwiftMVI/SwiftMVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMVIApp.swift
+//  SwiftMVI
+//
+//  Created by Marcel Kulina on 15.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftMVITests/SwiftMVITests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftMVITests.swift
+//  SwiftMVITests
+//
+//  Created by Marcel Kulina on 15.05.26.
+//
+
+import Testing
+
+struct SwiftMVITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
